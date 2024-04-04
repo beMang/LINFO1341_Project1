13919492e710c9b543fbbcab25052fa9f4f54efe
beMang <antonuttiadrien@gmail.com>
2024-04-04 11:43:51 +0200

    update tcp stats and add rename traces

--- scripts/tcp_retransmission.py
+++ scripts/tcp_retransmission.py
@@ -4,13 +4,18 @@ import numpy as np
 datas_4g_time, datas_4g_error = np.loadtxt('./plots/datas/tcp_retransmission4g.csv', delimiter=',', unpack=True, skiprows=1)
 datas_ethernet_time, datas_ethernet_error = np.loadtxt('./plots/datas/tcp_retransmissionethernet.csv', delimiter=',', unpack=True, skiprows=1)
 
+file_size = 49 #mb
+
 print("Ethernet stats :")
 print("Mean : {} retransmission/sec".format(np.sum(datas_ethernet_error)/datas_ethernet_time[-1]))
-print("Transmission time : {} sec\n".format(datas_ethernet_time[-1]))
+print("Transmission time : {} sec".format(datas_ethernet_time[-1]))
+print("Rate of transfer : {} mb/sec".format(file_size/datas_ethernet_time[-1]))
+print("\n")
 
 print("4G stats :")
 print("Mean : {} retransmission/sec".format(np.sum(datas_4g_error)/datas_4g_time[-1]))
 print("Transmission time : {} sec".format(datas_4g_time[-1]))
+print("Rate of transfer : {} mb/sec".format(file_size/datas_4g_time[-1]))
 
 plt.figure("TCP Retransmission")
 plt.bar(datas_4g_time, datas_4g_error, label='4G')

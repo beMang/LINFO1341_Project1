bf9aa5b6aaf45a74a0ae0b98e155782cd5adeaf7
beMang <antonuttiadrien@gmail.com>
2024-04-05 16:51:58 +0200

    add report and last stats

--- scripts/tcp_retransmission.py
+++ scripts/tcp_retransmission.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-datas_4g_time, datas_4g_error = np.loadtxt('./plots/datas/tcp_retransmission4g.csv', delimiter=',', unpack=True, skiprows=1)
-datas_ethernet_time, datas_ethernet_error = np.loadtxt('./plots/datas/tcp_retransmissionethernet.csv', delimiter=',', unpack=True, skiprows=1)
+datas_4g_time, datas_4g_error = np.loadtxt('./plots/datas/tcp_retransmission_4g.csv', delimiter=',', unpack=True, skiprows=1)
+datas_ethernet_time, datas_ethernet_error = np.loadtxt('./plots/datas/tcp_retransmission_ethernet.csv', delimiter=',', unpack=True, skiprows=1)
+datas_wifi_time, datas_wifi_error = np.loadtxt('./plots/datas/tcp_retransmission_wifi.csv', delimiter=',', unpack=True, skiprows=1)
 
 file_size = 49 #mb
 
@@ -10,6 +11,7 @@ print("Ethernet stats :")
 print("Mean : {} retransmission/sec".format(np.sum(datas_ethernet_error)/datas_ethernet_time[-1]))
 print("Transmission time : {} sec".format(datas_ethernet_time[-1]))
 print("Rate of transfer : {} mb/sec".format(file_size/datas_ethernet_time[-1]))
+
 print("\n")
 
 print("4G stats :")
@@ -17,7 +19,15 @@ print("Mean : {} retransmission/sec".format(np.sum(datas_4g_error)/datas_4g_time
 print("Transmission time : {} sec".format(datas_4g_time[-1]))
 print("Rate of transfer : {} mb/sec".format(file_size/datas_4g_time[-1]))
 
+print("\n")
+
+print("Wifi stats :")
+print("Mean : {} retransmission/sec".format(np.sum(datas_wifi_error)/datas_wifi_time[-1]))
+print("Transmission time : {} sec".format(datas_wifi_time[-1]))
+print("Rate of transfer : {} mb/sec".format(file_size/datas_wifi_time[-1]))
+
 plt.figure("TCP Retransmission")
 plt.bar(datas_4g_time, datas_4g_error, label='4G')
-plt.bar(datas_ethernet_time, datas_ethernet_error, label='4G')
+plt.bar(datas_ethernet_time, datas_ethernet_error, label='ethernet')
+plt.bar(datas_wifi_time, datas_wifi_error, label='wifi')
 plt.show()
